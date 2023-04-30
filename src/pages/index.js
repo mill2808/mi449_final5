a7eaf18dafc34b384fd4687374eb99dbb2fb2163
mill2808 <105799175+mill2808@users.noreply.github.com>
2023-04-30 12:33:47 -0400

    header styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,26 +8,18 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Image Reference: Unsplash.com
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-lg lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center dark:from-inherit lg:static lg:w-auto lg:p-4 lg:dark:bg-zinc-800/30">
+          Solar Radiation Dashboard
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto ">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https:unsplash.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Image by Unsplash
           </a>
         </div>
       </div>
